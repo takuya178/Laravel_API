ea7fdce474be610ffe9099d24cf69cbb8eddc46a
takuya178 <takuya.u1005@gmail.com>
2022-12-31 15:09:19 +0900

    create model

--- src/database/migrations/2022_12_31_053705_create_quotes_table.php
+++ src/database/migrations/2022_12_31_053705_create_quotes_table.php
@@ -13,14 +13,10 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('quotes', function (Blueprint $table) {
             $table->id();
-            //下記を追加
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();;
+            $table->string("text");
+            $table->string("author");
             $table->timestamps();
         });
     }
@@ -32,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('quotes');
     }
 };
